8ef8c67dbb9102d8d3d5ae5255cd94ba9e0014e4
Fabian Jacquet <tatou@MacBook-Pro-de-Fabian.local>
2020-08-17 23:32:10 +0200

    update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '572cb359b56ad9ae52f9c23d29d4b19a040af10d6635642e646a7caa7b96de717ce683bd797a92ce99e5929cc51e7d5f') { echo 'Installer verified'; } else { echo 'Installer corrupt. Check the sha284 here: https://getcomposer.org/download/'; unlink('composer-setup.php'); } echo PHP_EOL;"
 mkdir dep
 php composer-setup.php --install-dir=dep --filename=composer
 php -r "unlink('composer-setup.php');"
-./dep/composer
+./dep/composer i
