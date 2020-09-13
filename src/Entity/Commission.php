93abea3101f6f6939f8b20dac973814b58abfdbf
Kadrack <kadrack@hotmail.com>
2020-09-13 20:47:16 +0200

    Initial commit

--- src/Entity/Commission.php
+++ src/Entity/Commission.php
@@ -30,7 +30,7 @@ class Commission
     private $commission_name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $commission_role;
 
