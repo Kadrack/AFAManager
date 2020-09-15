c41acb8f639ae4d2c8bd3155e91a2f4473724fe9
Kadrack <kadrack@hotmail.com>
2020-09-15 17:47:01 +0200

    Initial commit

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -533,7 +533,6 @@ class ClubController extends AbstractController
         if ($form->isSubmitted() && $form->isValid())
         {
             $user->setPassword($this->passwordEncoder->encodePassword($user, $form['Password']->getData()));
-            $user->setUserClub($club);
             $user->setUserMember($member);
             $user->setUserStatus(1);
 
