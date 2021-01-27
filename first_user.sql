bd05c5eb0bc2289963f0c59cd327c89c59714c64
Kadrack <kadrack@hotmail.com>
2021-01-27 21:22:44 +0100

    Initial commit

--- first_user.sql
+++ first_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO `afamanager_user` (`id`, `user_join_member`, `user_join_club`, `login`, `user_firstname`, `user_real_name`, `roles`, `password`, `user_last_activity`, `user_status`) VALUES
-(1, NULL, NULL, 'Test', NULL, NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$aXhwOTE3TmE4WTJWdmtsQQ$9x0Y/FG6C9G5S/Hsaf7A6e1CqYsvJQRzpuDAnc/x0Z0', NULL, 1);
+INSERT INTO `afamanager_user` (`id`, `user_join_member`, `login`, `user_firstname`, `user_real_name`, `roles`, `password`, `user_last_activity`, `user_status`) VALUES
+(1, NULL, 'Test', NULL, NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$aXhwOTE3TmE4WTJWdmtsQQ$9x0Y/FG6C9G5S/Hsaf7A6e1CqYsvJQRzpuDAnc/x0Z0', NULL, 1);
