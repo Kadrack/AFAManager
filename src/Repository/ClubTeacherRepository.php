36928fa4790f855274c2d99b86a14e3ed12f352e
Kadrack <kadrack@hotmail.com>
2021-03-28 14:46:11 +0200

    Redefinition of start practice for easy update

--- src/Repository/ClubTeacherRepository.php
+++ src/Repository/ClubTeacherRepository.php
@@ -33,7 +33,6 @@ class ClubTeacherRepository extends ServiceEntityRepository
 
         return $qb->select('m.member_firstname AS Firstname', 'm.member_name AS Name', 'm.member_start_practice AS Starting')
             ->join(Member::class, 'm', 'WITH', $qb->expr()->eq('m.member_id', 't.club_teacher_member'))
-            ->join(MemberLicence::class, 'l', 'WITH', $qb->expr()->eq('m.member_first_licence', 'l.member_licence_id'))
             ->join(Club::class, 'c', 'WITH', $qb->expr()->eq('c.club_id', 't.club_teacher'))
             ->join(ClubHistory::class, 'h', 'WITH', $qb->expr()->eq('h.club_history_id', 'c.club_last_history'))
             ->where($qb->expr()->eq('t.club_teacher_title', 1))
